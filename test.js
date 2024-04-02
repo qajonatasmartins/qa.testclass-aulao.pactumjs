6a52c6d7449d54ae0ff61977d04b4cf23a736efb
Jonatas Faria @QAJONATASMARTINS <jonatas1503@hotmail.com>
2024-04-02 00:40:30 +0000

    .

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 // test.js
 const { spec } = require("pactum");
+const { allure } = require("allure-mocha/runtime");
 
 it("should get a response with status code 200", async () => {
+  allure.epic("Usuários");
+  allure.feature("Alterar usuário");
+  allure.story("Alterar por ID");
   await spec().get("http://httpbin.org/status/200").expectStatus(200);
 });
